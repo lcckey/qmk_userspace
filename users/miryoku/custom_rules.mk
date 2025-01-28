755e431144c671901b38a86a647ad31525b82c2f
lcckey <194552411+lcckey@users.noreply.github.com>
2025-01-28 01:35:48 +0000

    Update custom_rules.mk

--- users/miryoku/custom_rules.mk
+++ users/miryoku/custom_rules.mk
@@ -1,3 +1,11 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+VIA_ENABLE			= yes
+VIAL_ENABLE			= yes
+VIALRGB_ENABLE		= yes
+ENCODER_MAP_ENABLE	= no
+
+SPLIT_KEYBOARD		= yes
+RGBLIGHT_ENABLE		= no
+RGB_MATRIX_ENABLE	= yes
